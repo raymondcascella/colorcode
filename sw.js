1af279ec6fe4a01772b96714c721d8f0d4bcefc1
Raymond Cascella <raymondcascella@gmail.com>
2026-04-18 17:59:46 -0400

    feat: add cache-first service worker

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE = 'colorcode-v1';
+
+const SHELL = [
+  '/colorcode/',
+  '/colorcode/src/main.js',
+  '/colorcode/src/bag.js',
+  '/colorcode/src/board.js',
+  '/colorcode/src/game.js',
+  '/colorcode/src/rules.js',
+  '/colorcode/src/scoring.js',
+  '/colorcode/src/ui.js',
+  '/colorcode/manifest.json',
+  '/colorcode/icons/icon-192.svg',
+  '/colorcode/icons/icon-512.svg',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (!e.request.url.startsWith(self.location.origin)) return;
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
